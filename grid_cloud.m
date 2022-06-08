84ff76cb47ef73d62603fad4ee6e469f4bb1d454
David Russell <davidrussell327@gmail.com>
2022-06-08 12:57:17 -0400

    Much faster gridding (100x?)

--- grid_cloud.m
+++ grid_cloud.m
@@ -1,12 +1,22 @@
-function [grid_point_cloud, grid_labels_mtx] = grid_cloud(pc_data, pc_path, resolution)
+function [grid_point_cloud, grid_labels_mtx] = grid_cloud(pc_data, pc_path, resolution, force_reload)
+
+if nargin < 4
+    force_reload = true;
+end
+    
+function points = index_cloud(inds) 
+    points = pc_data(inds,:);
+end
+
 %% Dividing Point Cloud in Grid
 base_name = split(pc_path, '.');
 base_name = base_name(1);
 gc_data_path = strcat(base_name, '_grid_cloud.mat');
-if isfile(gc_data_path)
+gc_data_path = gc_data_path{1}; % Remove spurious cell array
+if isfile(gc_data_path) && ~force_reload
     %% Load Data
     disp("Loading Grid Cloud")
-    load(gc_data_path{1}, 'grid_point_cloud', 'grid_labels_mtx');
+    load(gc_data_path, 'grid_point_cloud', 'grid_labels_mtx');
 else
     %% Process Data
     disp("Generating Grid Cloud")
@@ -15,58 +25,48 @@ else
     grid_labels = [];
     nLabels = 1;
 
+    % TODO vectorize
     x_min = min(pc_data(:,1));
     x_max = max(pc_data(:,1));
 
     y_min = min(pc_data(:,2));
     y_max = max(pc_data(:,2));
 
-    xLimits = linspace(x_min, x_max, fix(x_max - x_min)); %fix((x_max - x_min)/nGrid));
-    yLimits = linspace(y_min, y_max, fix(y_max - y_min)); %fix((y_max - y_min)/nGrid));
-    % xLimits = linspace(x_min, x_max, fix((x_max - x_min)/resolution));
-    % yLimits = linspace(y_min, y_max, fix((y_max - y_min)/resolution));
-    % xLimits = linspace(x_min, x_max, 200);
-    % yLimits = linspace(y_min, y_max, 200);
-    disp('Grid Cloud Resolution X: ')
-    disp((x_max - x_min)/fix(x_max - x_min));
+    z_min = min(pc_data(:,3));
+    z_max = max(pc_data(:,3));
 
-    disp('Grid Cloud Resolution Y: ')
-    disp((y_max - y_min)/fix(y_max - y_min));
+    xLimits = linspace(x_min, x_max, fix((x_max - x_min)/resolution));
+    yLimits = linspace(y_min, y_max, fix((y_max - y_min)/resolution));
+    
+    num_x = fix((x_max - x_min)/resolution);
+    num_y = fix((y_max - y_min)/resolution);
+    disp('Grid Cloud Resolution X, Y: ' + resolution)
+    
+    spatial_extent = [[x_min, x_max];[y_min, y_max];[z_min, z_max]];
+    bins = pcbin(pointCloud(pc_data(:,1:3)), [num_x,num_y,1], spatial_extent);
+    points_per_bin = cellfun(@index_cloud, bins,'UniformOutput', false );
+    disp(points_per_bin{200,200})
 
-    if ~isemptgrid_cloudy(xLimits) && ~isempty(yLimits)
-        for k = progress(1:length(xLimits)-1)
-            if k < length(xLimits)-1
-                idx_x = (xLimits(k) <= pc_data(:,1)) & (pc_data(:,1) < xLimits(k+1));
-            elseif k == length(xLimits)-1
-                idx_x = (xLimits(k) <= pc_data(:,1)) & (pc_data(:,1) <= xLimits(k+1));
-            end
+    grid_point_cloud = zeros([size(pc_data,1), 4]);
+    grid_labels_mtx = zeros(num_x, num_y);
 
-            for j = 1:length(yLimits)-1
-                if j <length(yLimits)-1
-                    idx_y = (yLimits(j) <= pc_data(:,2)) & (pc_data(:,2) < yLimits(j+1));
-                elseif j == length(yLimits)-1
-                    idx_y = (yLimits(j) <= pc_data(:,2)) & (pc_data(:,2) <= yLimits(j+1));
-                end
-
-                idx = idx_x & idx_y;
-                grid_cloud = pc_data(idx,:);
-
-                % if ~isempty(gridCloud) && (size(gridCloud,1)>1)
-                grid_point_cloud = [grid_point_cloud; grid_cloud];
-
-                labels = ones(size(grid_cloud,1),1)*nLabels;
-                grid_labels = [grid_labels; labels];
-
-                grid_labels_mtx(j,k) = nLabels;
-
-                nLabels = nLabels + 1;
-                %end
-
-            end
+    num_points_added = 1;
+    index = 1;
+    for i=1:num_x
+        for j=1:num_y
+            current_points = points_per_bin{i,j};
+            num_points = size(current_points, 1);
+            labels = ones(num_points,1)*index;
+            
+            grid_point_cloud(num_points_added:num_points_added+num_points-1, 1:3) = current_points(:, 1:3);
+            grid_point_cloud(num_points_added:num_points_added+num_points-1, 4) = labels;
+            
+            grid_labels_mtx(i,j) = index;
+            num_points_added = num_points_added + num_points;
+            index = index+1;
         end
     end
 
-    grid_point_cloud = [grid_point_cloud grid_labels];
     save(gc_data_path, 'grid_point_cloud', 'grid_labels_mtx');
 end
 
